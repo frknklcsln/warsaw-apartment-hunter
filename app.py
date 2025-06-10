879b673821d714fbf29057e24e7d483def582633
Furkan Kilicaslan <frknklcsln5@gmail.com>
2025-06-10 04:53:38 +0200

    non-feasible message update

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ st.markdown("""
     
     .main-header h1 {
         font-size: 2.5rem;
-        font-weight: 200;
+        font-weight: 1200;
         margin: 0;
         text-shadow: 0 2px 4px rgba(0,0,0,0.3);
     }
